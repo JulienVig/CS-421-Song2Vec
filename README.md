ea75e6fb0dd881182c1ae46f43afa6c2d1123059
Julien <julien.vignoud@epfl.ch>
2021-06-10 12:49:10 +0200

    Refactor, README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Song2Vec
+
+Project realized in the context of the Machine Learning for Behavioral Data (CS-421) class of 2021. Project members are Beatriz Borges, Michel Manon and Vignoud Julien.
+
+## Project files
+
+* `classification_tasks.ipynb`: Model comparisons on classifications tasks
+* `clustering.ipynb`: Clustering and interpretation
+* `EDA.ipynb`: Exploratory data analysis
+* `hyperparam_finetuning.ipynb`: Song2Vec finetuning
+* `prediction_eval_and_plotly.ipynb`: 
+* `tags.ipynb`: tag scraping and processing
+* `utils.py`: script with common functions
+* `vector_artihmetic.ipynb`: exploratory notebook for operation on song embeddings
+* `visualizations.ipynb`: Song2Vec 2D visualizations
+
+
--- utils.py
+++ utils.py
@@ -12,7 +12,6 @@ def read_lastfm(zip_name = "lastfm-dataset-1K.zip"):
                         names=['user_id', 'timestamp', 'artist_id', 'artist_name', 
                         'track_id', 'track_name'],
                         dtype={0:'category', 2:'category',3:'category',4:'category',5:'category'})
-        # songs.columns = ['user_id', 'timestamp', 'artist_id', 'artist_name', 'track_id', 'track_name']
         
         #Read user csv
         user_file = folder +"userid-profile.tsv"
@@ -20,3 +19,29 @@ def read_lastfm(zip_name = "lastfm-dataset-1K.zip"):
         users = pd.read_csv(z.open(user_file), sep='\t', parse_dates=[4], dtype=column_types) \
                     .rename(columns={'#id': 'user_id'})
     return songs, users
+
+def build_vocab(model):
+    emb_vectors = {}
+    for n in model.wv.index_to_key:
+        emb_vectors[n] = model.wv[n]
+    return emb_vectors
+
+def get_embeddings(df, , **kwargs):
+    df = df.sort_values("timestamp")
+    df = df[~df.track_name.isna()]
+    df["song_id"]= df.artist_name.cat.codes.astype("int64") * df.track_name.nunique() \
+                        + df.track_name.cat.codes
+    print(f"Number of entries: {len(df)}")
+    assert len(df.query("song_id < 0")) == 0 #Check underflow
+    
+    document = df.groupby("user_id").agg(sentences=("song_id", list))
+    model = Word2Vec(document.sentences.values, , **kwargs)
+    
+    emb_vectors = build_vocab(model)
+    print(f"Number of song embeddings: {len(emb_vectors)}")
+    return df, emb_vectors, model
+
+def load_model(filename):
+    model = Word2Vec.load(filename)
+    emb_vectors = build_vocab(model)
+    return emb_vectors, model
